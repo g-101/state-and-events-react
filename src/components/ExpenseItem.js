1ccf71b4638321a28e8206c404cd770f8372a3a2
genesis-g101 <gyreplicant@gmail.com>
2022-11-20 16:03:39 -0300

    passando dados via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
 import './ExpenseItem.css';
 // Um componente é apenas uma função em javascript
 // Para usar um componente em qualquer lugar, deve-se exporta-lo.
-export const ExpenseItem = () => {
+export const ExpenseItem = props => {
+  // com o unico parametro recebemos todos os dados que foram passado a esse componente.
+  // o parametro props recebe um objeto com todos os atributos.
+  console.log(props);
+
   // Um componente retorna apenas um elemento por vez
   return (
+    //  para aplicar o css colocamos className. Aqui o JSX por debaixo dos panos é apenas javascript no final.
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{props.date.toLocaleString('en-GB', { timeZone: 'UTC' })}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
