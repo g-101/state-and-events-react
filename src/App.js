8ad0f282c5ad8392518d989616a99cd992567fe5
genesis-g101 <gyreplicant@gmail.com>
2022-11-20 15:33:33 -0300

    add o basico de css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ExpenseItem } from './components/ExpenseItem';
 
 function App() {
+  // para aplicar o css colocamos className. Aqui o JSX por debaixo dos panos é apenas javascript no final.
   return (
     <div>
       <h1>Hello World</h1>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,5 +3,13 @@ import './ExpenseItem.css';
 // Para usar um componente em qualquer lugar, deve-se exporta-lo.
 export const ExpenseItem = () => {
   // Um componente retorna apenas um elemento por vez
-  return <h2>Expense Item Aqui!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 };
