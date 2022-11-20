53ef3e4ab2b41cba5845ba03015c4aa55f055b0e
genesis-g101 <gyreplicant@gmail.com>
2022-11-20 12:27:51 -0300

    construindo o primeiro componente

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import './ExpenseItem.css';
+// Um componente é apenas uma função em javascript
+// Para usar um componente em qualquer lugar, deve-se exporta-lo.
+export const ExpenseItem = () => {
+  // Um componente retorna apenas um elemento por vez
+  return <h2>Expense Item Aqui!</h2>;
+};
