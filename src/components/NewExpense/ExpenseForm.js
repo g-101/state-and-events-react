68bc18780c8fcb6de16eb0ce15b855906273fc25
genesis-g101 <gyreplicant@gmail.com>
2022-11-22 16:18:09 -0300

    trabalhando com multiplos states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 export const ExpenseForm = props => {
+  // pode se ter multiplos states por componente
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
   const titleChangeHandler = event => {
     // aponta para o elemento em que o evento ocorre
     // ness caso ele esta olhando o valor que foi digitado no input.
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
   };
   return (
     <form>
@@ -15,11 +27,11 @@ export const ExpenseForm = props => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
