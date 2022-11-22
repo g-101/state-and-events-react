a0ae7885c72783890eb5beee572ecde6e2a7fa91
genesis-g101 <gyreplicant@gmail.com>
2022-11-21 21:46:57 -0300

    trabalhando com state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 import './ExpenseItem.css';
+import React, { useState } from 'react';
 import { Card } from '../UI/Card';
 import { ExpenseDate } from './ExpenseDate';
 // Um componente é apenas uma função em javascript
@@ -6,15 +7,29 @@ import { ExpenseDate } from './ExpenseDate';
 export const ExpenseItem = props => {
   // com o unico parametro recebemos todos os dados que foram passado a esse componente.
   // o parametro props recebe um objeto com todos os atributos.
-  console.log(props.expense);
-  const clickHandler = () => alert('Clicked!');
+
+  // useState é um hook que recebe um valor inicial
+  // e retorna um state e um callback que atualiza o state.
+  // hooks so podem ser utilizados dentro de componentes
+  const [title, setTitle] = useState(props.expense.title);
+  // você pode nomea-los como quiser, o que importa é a ordem
+  // é dessa forma dados são manipulados, usando o state.
+
+  // cada componente tem seu state que funciona em sua instância
+  // sem afetar outros componentes.
+
+  const clickHandler = () => {
+    // setTitle atualiza o valor do state
+    setTitle('Updated!');
+    // existem outra formas de manipular um state, alem de eventos: uma resposta http ou um timer expirado.
+  };
 
   return (
     // children são componentes dentro de outro custom componente.
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
       </div>
       {/* o react disponibiliza events listeners como propriedades começando com "on" */}
